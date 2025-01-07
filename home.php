974a2a18bf2cee52b1e38480c959b1f655e2d89a
charan3004 <charanramayanam004@gmail.com>
2025-01-07 22:27:44 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,9 @@
+<section class="py-3">
+    <div class="container">
+        <h3 class="text-center fw-bolder">Welcome to MRCET Student Clubs Website</h3>
+        <hr>
+        <div>
+            <?php include "welcome.html" ?>
+        </div>
+    </div>
+</section>
